3178417916856ef3c4b2b966a9094589ebab44f5
unknown <rathoreav007@gmail.com>
2024-05-17 16:39:58 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import About from "./components/about/About.jsx";
+import Skills from "./components/skills/Skills.jsx";
+import Services from "./components/services/Services.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Services />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
